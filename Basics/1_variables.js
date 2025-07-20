d6bbb346f1a0b0a3ac85143067aa805d971f925b
yadu-codex <yadu.chaudharyx@gmail.com>
2025-07-20 07:47:48 +0000

    datatypes and general info

---
+++ Basics/1_variables.js
@@ -0,0 +1,6 @@
+ const classId = 2345 // remains constant ,cannot be changed
+ let studentName ="yadu" // lets you rewrite it with every line of code
+ var rollNO = 34 //  ( block scope problem , aavoid using it )
+studentName ="manu"
+ console.table([studentName,rollNO]);
+ //console.log(accountCity);
---
+++ Basics/2datatype.js
@@ -0,0 +1,21 @@
+"use strict"// file will use only new version of code 
+// alert(3 + 3) , will not work , using nodejs not window browser
+// console.log(2 + 2); console.log("yadu");
+//dont use above type of structure ( make your code easy to read(readibility) and future proof)
+/*tc39.es original documattion of javascript
+will use - MDM website  as resource*/
+
+//  Datatypes =>
+/*
+ number => 2^52 range 
+ bigint => only if the number is to big
+ string => "",'' can use both but prefer double quot
+ boolean => trye/false
+ null => standalone value / shows output object when checked type of 
+ undefined => value not assigned
+ symbol => unique 
+ 
+ 
+ objects=> */
+
+
