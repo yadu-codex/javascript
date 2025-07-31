6f54449a80f29121893ffdc2c2183c861ecf7cce
yadu-codex <yadu.chaudharyx@gmail.com>
2025-07-31 22:37:20 +0000

    comparing datatype

---
+++ Basics/comparisionDatatypes.js
@@ -0,0 +1,15 @@
+/* comparision ( <,>,==,!=,===)
+Javascript dont allow comparision between differnt datatypes 
+//also (===) uses to also check if they are same datatype.
+*/
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0); also comparing nulls gets tricky
+// console.log(null >= 0);( only one shows true)
+// same goes for undefined 
+//shows every oen false
+
+// console.log("2" > 2);
+// console.log("2" == "2");
+
+
