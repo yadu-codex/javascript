13f1ed0ff0fbe99ce8662424c5f3210a203c98ea
yadu-codex <yadu.chaudharyx@gmail.com>
2025-08-01 00:42:58 +0000

    strings in detail

---
+++ Basics/5_string.js
@@ -0,0 +1,10 @@
+// strings
+const myNAme = "yadu"
+const classNAme = 12
+//console.log(myNAme + classNAme)
+// rather use backticks for string interpolation `
+//console.log(`my name is ${myNAme} and my class is ${classNAme}`); 
+
+
+const gameName = new String('yadu chaudhary');
+console.log(gameName);
